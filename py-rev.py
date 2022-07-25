1499377a385b5aae96d6903084771a6d4e339563
itayle23-meet <109908182+itayle23-meet@users.noreply.github.com>
2022-07-25 10:01:33 +0300

    Create py-rev.py

---
+++ py-rev.py
@@ -0,0 +1,21 @@
+def create_youtube_vid(title,description):
+	new_vid = {"title" : title , "description" : description , "likes" : 0 , "dislikes" : 0, "comments" : {}}
+	return new_vid
+
+def like(new_vid):
+    new_vid["likes"] += 1
+    return new_vid
+
+def dislike(new_vid):
+    new_vid["likes"] += 1
+    return new_vid
+
+def add_comment(new_vid, username, comment_text):
+	new_vid["comments"][username] = comment_text
+	return new_vid
+
+new_vid = create_youtube_vid("fun_video" , "this video is fun")
+like(new_vid)
+dislike(new_vid)
+add_comment(new_vid , "user" , "cool!")
+print(new_vid)
